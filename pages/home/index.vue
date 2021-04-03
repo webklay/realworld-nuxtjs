f5203af8ce0e903517c2866975d34c65f999bf09
webklay <lxz_mail168@163.com>
2021-04-04 00:57:17 +0800

    更新首页

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="home-page">
+    <div class="banner">
+      <div class="container">
+        <h1 class="logo-font">测试标题</h1>
+        <p>A place to share your knowledge.</p>
+      </div>
+    </div>
     <div class="container page">
       <div class="row">
-
         <div class="col-md-9">
           <div class="feed-toggle">
             <ul class="nav nav-pills outline-active">
